3c3e4b286ae4e9640a14c49d159cbcc043844613
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2021-10-06 16:35:58 -0700

    add JS functionality

---
+++ script.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('.js-form');
+const emailInput = document.querySelector('.js-input');
+const errorText = document.querySelector('.js__error-text');
+const errorIcon = document.querySelector('.js__error-icon');
+
+const valiedateEmail = function (input) {
+  const reg =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return reg.test(input.toLowerCase());
+};
+
+form.addEventListener('submit', (e) => {
+  if (
+    emailInput.value == null ||
+    !valiedateEmail(emailInput.value) ||
+    emailInput.value === ''
+  ) {
+    e.preventDefault();
+    errorText.innerHTML = 'Please provide a valid email';
+    errorIcon.style.display = 'block';
+  }
+});
