d041968dc8986621d550755b2d763cb0b10a53ea
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2021-10-07 10:24:27 -0700

    fix minor issues

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const errorIcon = document.querySelector('.js__error-icon');
 
 const valiedateEmail = function (input) {
   const reg =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/gi;
 
-  return reg.test(input.toLowerCase());
+  return reg.test(input);
 };
 
 form.addEventListener('submit', (e) => {
